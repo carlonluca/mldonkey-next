af57463808ce00c9f08568fc3e2bd545ef8ace0f
Luca Carlon <carlon.luca@gmail.com>
2023-08-06 11:57:38 +0200

    Fixed warnings.

--- mldonkey-next-backend/src/main.ts
+++ mldonkey-next-backend/src/main.ts
@@ -1,14 +1,11 @@
-import * as net from 'net'
 import { logger } from './core/MLLogger'
-
 import WebSocket from 'ws'
-import { IncomingMessage } from 'http'
 import { MLBridgeManager } from './core/MLBridgeManager'
 
 const wss = new WebSocket.Server({ port: 8080 });
 const bridgeManager = new MLBridgeManager();
 
-wss.on('connection', (ws: WebSocket, req: IncomingMessage) => {
+wss.on('connection', (ws: WebSocket /* , _req: IncomingMessage */) => {
     console.log("Client connected")
     bridgeManager.clientConnected(ws)
 
@@ -16,7 +13,7 @@ wss.on('connection', (ws: WebSocket, req: IncomingMessage) => {
         console.log('Client disconnected')
         bridgeManager.clientDisconnected(ws)
     })
-    ws.on("error", (ws: WebSocket, err: Error) => {
+    ws.on("error", (_ws: WebSocket, err: Error) => {
         logger.warn(`Client failed: ${err.message}`)
     })
 })
