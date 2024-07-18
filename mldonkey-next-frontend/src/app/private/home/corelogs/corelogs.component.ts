8ce47af221bcbae59106185d5760ca50366c0891
Luca Carlon <carlon.luca@gmail.com>
2024-07-18 07:53:15 +0200

    Add a way to filter logs.

--- mldonkey-next-frontend/src/app/private/home/corelogs/corelogs.component.ts
+++ mldonkey-next-frontend/src/app/private/home/corelogs/corelogs.component.ts
@@ -21,13 +21,37 @@
  * Company: -
  * Date: 2024.07.07
  */
-
 import { Component, ElementRef, OnDestroy, OnInit, ViewChild, Renderer2 } from '@angular/core'
 import { interval } from 'rxjs'
 import { webSocket, WebSocketSubject } from 'rxjs/webSocket'
 import { environment } from 'src/environments/environment'
 import { ScrollToBottomDirective } from './scroll'
-import { faPersonWalkingArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { faArrowUp, faPersonWalkingArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { MLSubscriptionSet } from 'src/app/core/MLSubscriptionSet'
+import { uiLogger } from 'src/app/core/MLLogger'
+
+class LogLine {
+    timestamp: Date
+    tag: string
+    text: string
+
+    static parse(line: string): LogLine | null {
+        const logPattern = /(\d{4}\/\d{2}\/\d{2} \d{2}:\d{2}:\d{2}) \[(\w+)\] (.+)/
+        const match = line.match(logPattern)
+        if (match) {
+            const timestamp = new Date(match[1])
+            const tag = match[2]
+            const text = match[3]
+            return {
+                timestamp,
+                tag,
+                text
+            }
+        }
+        else
+            return null
+    }
+}
 
 @Component({
     selector: 'app-corelogs',
@@ -39,11 +63,16 @@ export class CorelogsComponent implements OnInit, OnDestroy {
     @ViewChild(ScrollToBottomDirective) scrollDirective: ScrollToBottomDirective
 
     faPersonWalkingArrowRight = faPersonWalkingArrowRight
+    faArrowUp = faArrowUp
 
     lastId = 0
-    buffer: string[] = []
+    buffer: LogLine[] = []
+    toProcess: LogLine[] = []
     websocket: WebSocketSubject<string> | null = null
     follow = true
+    subscribed = new MLSubscriptionSet()
+    showWaitingMessage = true
+    selectedRange = "1hour"
 
     constructor(private renderer: Renderer2) { }
 
@@ -53,18 +82,12 @@ export class CorelogsComponent implements OnInit, OnDestroy {
             deserializer: (e: MessageEvent) => e.data,
             serializer: (b: string) => b
         })
-        this.websocket.subscribe({
+        this.subscribed.add(this.websocket.subscribe({
             next: (data: string) => this.handleLine(data)
-        })
-        interval(500).subscribe(() => {
-            this.buffer.forEach(e => {
-                const paragraph = this.renderer.createElement('p')
-                const textNode = this.renderer.createText(e)
-                this.renderer.appendChild(paragraph, textNode)
-                this.renderer.appendChild(this.scrollContainer.nativeElement, paragraph)
-            })
-            this.buffer = []
-        })
+        }))
+        this.subscribed.add(interval(500).subscribe(() => {
+            requestAnimationFrame(() => this.processPendingLogs())
+        }))
     }
 
     ngOnDestroy(): void {
@@ -72,16 +95,87 @@ export class CorelogsComponent implements OnInit, OnDestroy {
     }
 
     handleLine(line: string) {
-        this.buffer.push(line)
+        const l = LogLine.parse(line)
+        if (l) {
+            this.toProcess.push(l)
+            this.buffer.push(l)
+            uiLogger.debug("To process:", this.toProcess.length)
+        }
     }
 
     onScroll(_event: WheelEvent) {
-        this.scrollDirective.follow = false
-        this.follow = false
+        this.dontFollow()
     }
 
     doFollow() {
         this.scrollDirective.follow = true
         this.follow = true
+        this.scrollToBottom()
+    }
+
+    scrollToTop() {
+        this.dontFollow()
+        this.scrollContainer.nativeElement.scrollTop = 0
+    }
+
+    scrollToBottom() {
+        if (this.scrollDirective)
+            this.scrollDirective.scrollToBottom()
+    }
+
+    dontFollow() {
+        this.scrollDirective.follow = false
+        this.follow = false
+    }
+
+    isAtTop() {
+        if (!this.scrollContainer)
+            return true
+        return this.scrollContainer.nativeElement.scrollTop === 0
+    }
+
+    selectedRangeChanged() {
+        this.toProcess = this.buffer
+        const containerElement = this.scrollContainer.nativeElement
+        while (containerElement.firstChild)
+            this.renderer.removeChild(containerElement, containerElement.firstChild)
+    }
+
+    processPendingLogs() {
+        const since = new Date()
+        switch (this.selectedRange) {
+            case "all":
+                break
+            case "1year":
+                since.setMonth(since.getMonth() - 12)
+                break
+            case "1month":
+                since.setMonth(since.getMonth() - 1)
+                break
+            case "1week":
+                since.setHours(since.getHours() - 24 * 7)
+                break
+            case "1day":
+                since.setHours(since.getHours() - 24)
+                break
+            case "1hour":
+                since.setHours(since.getHours() - 1)
+                break
+        }
+        for (var i = 0; i < this.toProcess.length; i++) {
+            const e = this.toProcess[i]
+            if (e.timestamp >= since || this.selectedRange === "all") {
+                this.showWaitingMessage = false
+                const paragraph = this.renderer.createElement('p')
+                const textNode = this.renderer.createText(e.timestamp + " " + e.text)
+                this.renderer.appendChild(paragraph, textNode)
+                this.renderer.appendChild(this.scrollContainer.nativeElement, paragraph)
+            }
+        }
+        this.toProcess = []
+        uiLogger.debug("Done:", this.toProcess.length)
+
+        if (this.toProcess.length > 0)
+            requestAnimationFrame(() => this.processPendingLogs())
     }
 }
--- mldonkey-next-frontend/src/app/private/home/corelogs/scroll.ts
+++ mldonkey-next-frontend/src/app/private/home/corelogs/scroll.ts
@@ -14,21 +14,12 @@ export class ScrollToBottomDirective implements AfterContentChecked {
             this.scrollToBottom()
     }
 
-    private scrollToBottom(): void {
+    scrollToBottom(): void {
         try {
             this.el.nativeElement.scrollTop = this.el.nativeElement.scrollHeight;
         }
         catch (err) {
             console.error('Scroll to bottom failed:', err);
         }
-    }
-
-    private isScrolledToBottom(): boolean {
-        const threshold = 5
-        const scrollTop = this.el.nativeElement.scrollTop
-        const scrollHeight = this.el.nativeElement.scrollHeight
-        const clientHeight = this.el.nativeElement.clientHeight
-        uiLogger.debug("scroll:", scrollHeight, scrollTop, clientHeight, threshold)
-        return scrollHeight - scrollTop - clientHeight - threshold < 1
     }
 }
