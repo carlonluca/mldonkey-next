1f29fe8082ff80896ec8187b40088dde93c00914
Luca Carlon <carlon.luca@gmail.com>
2025-09-08 14:04:23 +0200

    Updated icon.

--- app/update_icon.sh
+++ app/update_icon.sh
@@ -3,7 +3,7 @@
 set -e
 
 wget "https://github.com/sterlp/svg2png/releases/download/v1.2.1/svg2png-1.2.1.jar"
-java -jar svg2png-1.2.1.jar -f ../icon.svg -o android/res -c svg2png_android.json
+java -jar svg2png-1.2.1.jar -f ../icon_full.svg -o android/res -c svg2png_android.json
 #cp android/res/drawable-xxxhdpi/icon_24dp.png assets/icon_96.png
 #cp ../icon.svg assets/
 
