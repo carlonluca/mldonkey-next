47ad64f9bd7aa607be3e39b3ab4a3be68d4ecc68
Luca Carlon <carlon.luca@gmail.com>
2026-06-01 13:58:17 +0200

    Updated angular.

--- mldonkey-next-frontend/src/main.ts
+++ mldonkey-next-frontend/src/main.ts
@@ -1,6 +1,7 @@
+import { provideZoneChangeDetection } from "@angular/core";
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
 import { AppModule } from './app/app.module'
 import { uiLogger } from './app/core/MLLogger'
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()], })
   .catch(err => uiLogger.error(err))
